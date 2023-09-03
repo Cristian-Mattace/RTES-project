cca4d99d12bf0dfad039d27855907b3e2c163bcf
Cristian Mattace <cristianmattace5@gmail.com>
2023-09-03 14:40:13 +0200

    Update main.cpp and delete SinglyLinkedList

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
-#include "src/singly-linked-list/SinglyLinkedList.h"
+#include "src/singly-linked-list/StaticLinkedList.h"
+
 
 int main() {
-    SinglyLinkedList<int> list;
-
+    StaticLinkedList<int> list;
+    
+    list.push(1, 1);
+    list.push(2, 2);
+    list.push(88, 88);
     list.push(3, 3);
-    list.push(2, 1);
-
+    list.push(4, 4);
+    
     int a = 0;
     list.pull(a);
-    std::cout << a << std::endl;
+    std::cout << a << std::endl; 
+
     return 0;
 }
