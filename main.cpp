2c7e28513b0e7e28937a5400bf03d49599428f3c
Cristian Mattace <cristianmattace5@gmail.com>
2023-09-04 18:15:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,56 @@
 #include <iostream>
 #include "src/singly-linked-list/SinglyLinkedList.h"
+#include "src/singly-linked-list/GroupsLinkedList.h"
 
 int main() {
-    SinglyLinkedList<int> list;
+    GroupsLinkedList<int> groupsList;
 
-    list.push(3);
-    list.push(2);
+    groupsList.push(1);
+    groupsList.push(11, 1);
+    groupsList.push(21, 1);
 
-    int a = 0;
-    list.pull(a);
-    std::cout << a << std::endl;
+    groupsList.push(2);
+    groupsList.push(12, 2);
+    groupsList.push(22, 2);
+
+    groupsList.push(3);
+    groupsList.push(13, 3);
+    groupsList.push(23, 3);
+
+    groupsList.push(4);
+    groupsList.push(14, 4);
+    groupsList.push(24, 4);
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    int value = 0;
+    groupsList.pull(value, 2);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value, 4);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value, 3);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value, 1);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value);
+    std::cout << value << std::endl;
+
+    groupsList.pull(value);
+    std::cout << value << std::endl;
+
     return 0;
 }
