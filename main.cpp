d21c3bb7b00a2905fee7b3e63a3fd501ef81959f
Fede <federico.canali00@gmail.com>
2023-09-05 10:39:42 +0200

    Introduced the verbose parameter

--- main.cpp
+++ main.cpp
@@ -5,28 +5,13 @@ int main() {
     StaticLinkedList<int> list;
 
     list.push(3, 3);
-    list.toString();
-
     list.push(2, 2);
-    list.toString();
-
     list.push(1, 1);
-    list.toString();
-
     list.push(4, 4);
-    list.toString();
-
     list.push(6, 6);
-    list.toString();
-
     list.push(5, 5);
-    list.toString();
-
     list.push(0, 0);
-    list.toString();
-
     list.push(88, 5);
-    list.toString();
 
     int a = 0;
     list.pull(a);
